829f2fb1685048798644a71bba1e62113b296eba
Gustavo Omonte Acevedo <GustavoGOAweb@gmail.com>
2021-07-28 16:16:18 -0400

    upload images and adding components

---
+++ src/components/Hero/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../Navbar';
+import { HeroContainer } from './HeroElements';
+
+
+const Hero = () => {
+    return (
+        <>
+           <HeroContainer>
+               <Navbar></Navbar>
+           </HeroContainer>
+        </>
+    )
+}
+
+export default Hero;
